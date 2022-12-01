835be9cb13e6ea4c4fc21c89ff469ea527ed72f3
Lorenzobenitez87 <105927927+Lorenzobenitez87@users.noreply.github.com>
2022-12-01 08:38:15 +0100

    exercice lmj

--- my-app/src/components/Banner.js
+++ my-app/src/components/Banner.js
@@ -1,14 +1,8 @@
 import '../styles/Banner.css'
-import logo from '../assets/logo.png'
+//import logo from '../assets/logo.png'
 
-function Banner() {
-    const title = "La maison jungle"
-    return (
-    <div className='lmj-banner'>
-        <img src={logo} alt='Logo Kasa' className='lmj-logo' />
-        <h1 className='lmj-title'>{title.toUpperCase()}</h1>
-        </div>
-        )
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
 }
 
 export default Banner
--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from 'react'
 import Banner from '../components/Banner'
+import logo from '../assets/logo.png'
 import Cart from '../components/Cart'
+import Footer from '../components/Footer'
 import ShoppingList from '../components/ShoppingList'
+import '../styles/Layout.css'
 
 function Home() {
-    return <div>
-    <Banner />
-    <Cart />
-    <ShoppingList  />
-</div>
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+			<Footer />
+		</div>
+	)
 }
 
 export default Home
